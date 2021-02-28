13cb5a78ab5d17b547a3276f2fb9c29482ecdc6f
Andrew Wippl <ajwippl@gmail.com>
2021-02-28 15:44:46 -0500

    my first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $posts = [];
+
+        if (auth()->user()->token) {
+
+            if (auth()->user()->token->hasExpired()) {
+                return redirect('/oauth/refresh');
+            }
+
+            $response = Http::withHeaders([
+                'Accept' => 'application/json',
+                'Authorization' => 'Bearer ' . auth()->user()->token->access_token
+            ])->get(config('services.oauth_server.uri') . '/api/posts');
+
+            if ($response->status() === 200) {
+                $posts = $response->json();
+            }
+
+            if ($response->status() === 401) {
+                auth()->user()->token->delete();
+                return redirect('/home');
+            }
+        }
+
+        return view('home', [
+            'posts' => $posts
+        ]);
+    }
+}
